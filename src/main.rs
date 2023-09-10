573e571002ca2e2cf0381f65630b6cbab62403d5
vrinta <info@vrinta.com>
2023-09-11 00:53:09 +0300

    Initial model

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#[derive(Debug, Clone)]
+struct Cell {
+    x_position: u16,
+    y_position: u16,
+    value: CellKind,
+}
+
+impl Cell {
+    fn new(x_position: u16, y_position: u16, value: CellKind) -> Self {
+        Cell {
+            x_position,
+            y_position,
+            value,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum CellKind {
+    Empty,
+    Bomb,
+    Number(u8),
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut c: Cell = Cell {
+        x_position: 0,
+        y_position: 0,
+        value: CellKind::Empty,
+    };
 }
